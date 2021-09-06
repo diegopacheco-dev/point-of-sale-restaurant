183ba694f29a488d267c3b3a1e521b34acafb703
ericksonder <ericksonder017@gmail.com>
2021-09-06 14:18:12 -0500

    Feat: añadiendo el organismo ProductosContainer

---
+++ src/components/03-organisms/ProductsContainer/ProductsContainer.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import Heading from "../../01-atoms/Heading/Heading";
+import ProductCard from "../../02-molecules/ProductCard/ProductCard";
+import './styles.css'
+
+const ProductsContainer = ({ products = [], categoriaGlobal = "drinks" }) => {
+  const [cargando, setCargando] = useState(false);
+
+  products = [
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+    {
+      name: "Producto",
+      img:
+        "https://i.picsum.photos/id/755/200/200.jpg?hmac=fgsDUz8GLl3UPtHhHlMIabU9V8LhbOPCwYGzrrn6CyU",
+      price: 60,
+      stock: 15,
+    },
+  ];
+
+  return (
+    <div className="vertical-space-2">
+      <Heading>Productos</Heading>
+
+      {!categoriaGlobal ? (
+        <p>Selecciona una categoría</p>
+      ) : cargando ? (
+        <p>Cargando...</p>
+      ) : (
+        <div className="products-container">
+          {products.map((product) => {
+            return (
+              <ProductCard
+                photo={product.img}
+                name={product.name}
+                price={product.price}
+                stock={product.stock}
+                action={() => alert("añadiendo producto")}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductsContainer;
---
+++ src/components/03-organisms/ProductsContainer/ProductsContainer.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ProductsContainer from './ProductsContainer'
+
+export default {
+    title: "Organisms/ProductsContainer",
+    component: ProductsContainer
+}
+
+export const primary = () => <ProductsContainer />
