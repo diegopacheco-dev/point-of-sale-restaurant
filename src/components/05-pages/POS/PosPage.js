c07601b40ba22d37d4a6e9764ba89ee002b16789
ericksonder <ericksonder017@gmail.com>
2021-09-26 11:48:44 -0500

    Avanzando con el template Point of Sale

--- src/components/05-pages/POS/PosPage.js
+++ src/components/05-pages/POS/PosPage.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import ButtonIcon from "../../01-atoms/Buttons/ButtonIcon/ButtonIcon";
+import "./styles.css";
 
 const PosPage = (props) => {
-    return (
-        <div>
-            Punto de venta page
-        </div>
-    )
-}
+  const [showShoppingCart, setShowShoppingCart] = useState(false);
+  return (
+    <div className="pos-template">
 
-export default PosPage
+      <div className="btn-showShoppingCart">
+        <button onClick={() => setShowShoppingCart(true)}>Mostrar</button>
+      </div>
+
+
+      <div className="pos-template__main-container">
+        <div className="header">
+            <div style={{height: '10rem', backgroundColor: 'lightgray'}}>
+                <h2>Header</h2>
+            </div>
+        </div>
+
+        <div className="body">
+            <div style={{height: '45rem', backgroundColor: 'lightgray'}}>
+                <h2>Body</h2>
+            </div>
+        </div>
+
+      </div>
+
+
+      <div
+        className={`pos-template__shoppingCart ${showShoppingCart && "show"}`}
+      >
+        <h2>Carrito</h2>
+        <div className="btn-shoppingCart">
+          <ButtonIcon nameIcon="x" action={() => setShowShoppingCart(false)} />
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+};
+
+export default PosPage;
