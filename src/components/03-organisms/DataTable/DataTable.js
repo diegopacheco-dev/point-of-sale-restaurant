8fbbbd7a22c69249da9256efaf2f18b28638e25c
Diego <ericksonder017@gmail.com>
2021-10-01 23:11:10 -0500

    Feat: añadiendo el template tablePageTemplate y usandolo en el page PedidosPage

--- src/components/03-organisms/DataTable/DataTable.js
+++ src/components/03-organisms/DataTable/DataTable.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Heading from "../../01-atoms/Heading/Heading";
 import { MDBDataTableV5 } from "mdbreact";
+import "./styles.css";
 
 const DataTable = ({ title, data, loading = true }) => {
   return (
-    <div className="vertical-space-1">
+    <div className="data-table vertical-space-1">
       {loading ? (
         <p>Cargando...</p>
       ) : data?.rows.length > 0 ? (
         <>
           <Heading>{title}</Heading>
-          <MDBDataTableV5 striped hover data={data} searchTop searchBottom={false}/>
+          <div className="data-table__wrapper">
+            <MDBDataTableV5
+              responsive
+              striped
+              hover
+              data={data}
+              searchTop
+              searchBottom={false}
+            />
+          </div>
         </>
       ) : (
         <p>Tabla vacía</p>
--- src/components/05-pages/Pedidos/PedidosPage.js
+++ src/components/05-pages/Pedidos/PedidosPage.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React from "react";
+import DataTable from "../../03-organisms/DataTable/DataTable";
+import ShowStats from "../../03-organisms/ShowStats/ShowStats";
+import "../../04-templates/tablePage-template/styles.css";
 
 const PedidosPage = () => {
-    return (
-        <div>
-            <h1>Pedidos</h1>
-        </div>
-    )
-}
+  const data = {
+    columns: [
+      {
+        label: "Nombre",
+        field: "nombre",
+        sort: "asc",
+      },
+      {
+        label: "Fecha",
+        field: "fecha",
+        sort: "asc",
+      },
+      {
+        label: "Cantidad Platos",
+        field: "cantidad_platos",
+        sort: "asc",
+      },
+      {
+        label: "Total",
+        field: "costo_total",
+        sort: "asc",
+      },
+      {
+        label: "Monto Pagado",
+        field: "monto_pagado",
+        sort: "asc",
+      },
+      {
+        label: "Entrega",
+        field: "estado_entrega",
+        sort: "asc",
+      },
+      {
+        label: "Acciones",
+        field: "acciones",
+        sort: "asc",
+      },
+    ],
+    rows: [
+      {
+        nombre: "Diego",
+        fecha: "10/05/2020",
+        cantidad_platos: 30,
+        costo_total: `S/ ${50}`,
+        monto_pagado: `S/ ${30}`,
+        estado_entrega: "entregado",
+        acciones: <button>Hola mundo</button>,
+      },
+      {
+        nombre: "Diego",
+        fecha: "10/05/2020",
+        cantidad_platos: 30,
+        costo_total: `S/ ${50}`,
+        monto_pagado: `S/ ${30}`,
+        estado_entrega: "entregado",
+        acciones: <button>Hola mundo</button>,
+      },
+      {
+        nombre: "Diego",
+        fecha: "10/05/2020",
+        cantidad_platos: 30,
+        costo_total: `S/ ${50}`,
+        monto_pagado: `S/ ${30}`,
+        estado_entrega: "entregado",
+        acciones: <button>Hola mundo</button>,
+      },
+      {
+        nombre: "Diego",
+        fecha: "10/05/2020",
+        cantidad_platos: 30,
+        costo_total: `S/ ${50}`,
+        monto_pagado: `S/ ${30}`,
+        estado_entrega: "entregado",
+        acciones: <button>Hola mundo</button>,
+      },
+    ],
+  };
 
-export default PedidosPage
+  return (
+    <div className="table-page-template">
+      <div className="table-page-template__col-1">
+        <div className="header">
+          <ShowStats />
+        </div>
+        <div className="body">
+          <DataTable title="Lista de Pedidos" data={data} loading={false} />
+        </div>
+      </div>
+      <div className="table-page-template__col-2">
+        {/* <div style={{ height: "45rem", backgroundColor: "lightgray" }}>
+          <h2>Aside</h2>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default PedidosPage;
